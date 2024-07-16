70e8301c7dbe01531ccd059125847a827151bd41
amirhassan <amirhassan.hamzan@gmail.com>
2024-07-16 13:21:32 +0800

    Update default port to 9999

--- vue-crash-course-2024/vite.config.js
+++ vue-crash-course-2024/vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  server: {
+    port: 9999
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
